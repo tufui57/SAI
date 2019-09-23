65b81ba5e06bda6fc5ffbf591bda50e5f5e155c9
DESKTOP-3U6270B\nomur <nommi038@student.otago.ac.nz>
2019-09-23 16:45:20 +1200

    changes for SAI calculation

--- SAI package/04_calculate_SAIcl_neighbourhood_outlier.R
+++ SAI package/04_calculate_SAIcl_neighbourhood_outlier.R
@@ -10,15 +10,13 @@ source(".\\SAI\\SAI package\\F_SpatialAvailabilityIndex.R")
 ###################################################################################################
 
 calc_SAI <- function(
-  data1, # data.frame containing data of grid cells to calcualte SAI for, and search analogous climates.
-  data2,
+  data1, # data.frame containing data of grid cells to calcualte SAI for.
+  data2, # data.frame containing data of grid cells to search analogous climates from.
   climateNames, # Vector. Column names of environmental variables to search for analogous environment conditions
   coordinateNames, # Vector with two elements. Column names of coodinate in data1 & data2
   climateRangeStep = 10, # Integer. Breaks of cliamte range breaths.
-  neighbourhood.size, # Size of neighbourhood area to search analogous climates (km)
+  neighbourhood.size = NULL, # Size of neighbourhood area to search analogous climates (km)
   # Unit of NZTM is meter. This function might need an option for data whose coordinate unit is degree.
-  
-  no.neighbourhood, # True/False; T = no limit for neighbourhood to search analogous climates, F = give neighbourhood.size
   standardize = F, # Standardize climate data
   ordination = F, # use of ordination axes instead of climate variables
   outlierRemoval = F, # remove climate outliers from data to search analogous climates from.
@@ -54,59 +52,55 @@ calc_SAI <- function(
     ######################################################################################
     # Prepare the neighbourhood square of the target cell
     ######################################################################################
-    if(no.neighbourhood == TRUE){
+    
+    ## If data1 and data2 are the same 
+    if(sum(data1[,3] != data2[,3]) == 0){
       
       neighbour.window <- data2
+
+      if(is.null(neighbourhood.size)){
+
+      }else{
+        
+        # Prepare a neighbourhood square to search analogous climate from.
+        # IF neighbourhood.size = 20 (km), the radius of the neighbourhood square, "a", is 10000 m (= 10km = 20km/2)
+        a <- neighbourhood.size * 1000 / 2
+        dat.x <- analogousCells_in_single_variable(p, data2, a, coordinateNames[1])
+        neighbour.window <- analogousCells_in_single_variable(p, dat.x, a, coordinateNames[2])
+      }
+      
+      ranges <- lapply(climateNames, get_radius_size, dat = neighbour.window)
+      names(ranges) <- climateNames
       
     }else{
       
-      # Prepare a neighbourhood square to search analogous climate from.
-      # IF neighbourhood.size = 20 (km), the radius of the neighbourhood square, "a", is 10000 m (= 10km = 20km/2)
-      a <- neighbourhood.size * 1000 / 2
-      dat.x <- analogousCells_in_single_variable(p, data2, a, coordinateNames[1])
-      neighbour.window <- analogousCells_in_single_variable(p, dat.x, a, coordinateNames[2])
-    }
-    
-    ######################################################################################
-    ### Outlier removal
-    ###################################################################################### 
-    if(outlierRemoval == TRUE){
-      
-      # Remove outliers from the neighbourhood
-      neighbour.window <- lapply(climateNames, remove_outliers, 
-                       outlierPercent = outlierPercent, dat = neighbour.window 
-                       )
-      
-      # If a climate values of p is an outlier, 
-      # the outlying value must be replaced with the min or max value of non-outlying values.
-      res <- list()
-      
-      for(j in climateNames){
-        res[[j]] <- check_outliers(p, data2, j, outlierPercent)
+      ## If data1!= data2
+      if(is.null(neighbourhood.size)){
+        
+        # Combine cells of data1 and data2 within the neighbourhood
+        neighbour.window.data1_2 <- rbind(neighbour.window[c(coordinateNames, climateNames)], 
+                                          data1[c(coordinateNames, climateNames)])
+        ranges <- lapply(climateNames, get_radius_size, dat = neighbour.window.data1_2)
+        names(ranges) <- climateNames
+        
+      }else{
+        
+        # Prepare a neighbourhood square to search analogous climate from.
+        # IF neighbourhood.size = 20 (km), the radius of the neighbourhood square, "a", is 10000 m (= 10km = 20km/2)
+        a <- neighbourhood.size * 1000 / 2
+        dat.x <- analogousCells_in_single_variable(p, data2, a, coordinateNames[1])
+        neighbour.window <- analogousCells_in_single_variable(p, dat.x, a, coordinateNames[2])
+        
+        # Combine cells within the neighbourhood and the target point
+        neighbour.window.data2_p <- rbind(neighbour.window[c(coordinateNames, climateNames)], 
+                                          p[c(coordinateNames, climateNames)])
+        ranges <- lapply(climateNames, get_radius_size, dat = neighbour.window.data2_p)
+        names(ranges) <- climateNames
       }
       
-      # Replace the values
-      p[, climateNames] <- unlist(res)
-      
-      
-      ranges <- lapply(climateNames, get_radius_size, 
-                       dat = neighbour.window, climateRangeStep = climateRangeStep)
-      names(ranges) <- climateNames
-    
-      }else{
-      
-        ######################################################################################
-    # Prepare climate ranges of cells within the neighbourhood
-    ###################################################################################### 
-    
-    # Calcualte steps of climate range breaths for the grid cells within the neighbourhood + the target cell
-    scores.int <- rbind(neighbour.window[,c(coordinateNames, climateNames)], p[,c(coordinateNames, climateNames)])
-    ranges <- lapply(climateNames, get_radius_size, 
-                     dat = scores.int, climateRangeStep = climateRangeStep)
-    names(ranges) <- climateNames
       
     }
-    
+
     
     ###################################################################################### 
     ### SAI calculation
@@ -134,14 +128,13 @@ load(".\\Scores_Acaena_landcover5km.data")
 climateNames <- c("bioclim1", "bioclim6", "bioclim12", "bioclim15")
 
 ### SAI within i km neighbourhood
-for(i in c(20,50,100)){
+for(n in c(20,50,100)){
   
   sai.i <- calc_SAI(data1 = scores,
                     data2 = scores,
                     climateNames = climateNames,
                     coordinateNames = c("x","y"),
-                    neighbourhood.size = i, 
-                    no.neighbourhood = F
+                    neighbourhood.size = n
                     )
   # Save
   save(sai.i, file = paste("SAI_5km_currentInCurrent_", i,"kmNeighbourhood.data", sep=""))
@@ -149,13 +142,11 @@ for(i in c(20,50,100)){
 }
 
 ### SAI within Whole NZ
-sai.i <- calc_SAI(scores, 
-                  scores,
-                 climateNames,
-                 coordinateNames = c("x","y"),
-                 neighbourhood.size = 5000, 
-                 no.neighbourhood = T
-                 )
+sai.i <- calc_SAI(data1 = scores,
+                  data2 = scores,
+                  climateNames = climateNames,
+                  coordinateNames = c("x","y")
+                  )
 save(sai.i, file = "SAIcc_NZ_4var.data")
 
 ###### Outlier removal
@@ -163,10 +154,9 @@ save(sai.i, file = "SAIcc_NZ_4var.data")
 for(i in c(1, 2.5, 5)){
   # Whole NZ
   sai.i <- calc_SAI(data1 = scores,
+                    data2 = scores,
                     climateNames = climateNames,
                     coordinateNames = c("x","y"),
-                    neighbourhood.size = 5000, 
-                    no.neighbourhood = T,
                     outlierRemoval = T,
                     outlierPercent = i
   )
@@ -189,10 +179,9 @@ for(i in c(20,50,100)){
   ### i km neighbourhood window
   sai.i <- calc_SAI(scores.lgm,
                     scores.lgm,
-                    climateNames,
+                    climateNames = climateNames,
                     coordinateNames = c("x","y"),
-                    neighbourhood.size = i, 
-                    no.neighbourhood = F
+                    neighbourhood.size = i
   )
   # Save
   save(sai.i, file = paste("SAI_5km_LGMInLGM_", i,"kmNeighbourhood.data", sep=""))
@@ -203,10 +192,8 @@ for(i in c(20,50,100)){
 sai.i <- calc_SAI(scores.lgm, 
                   scores.lgm, 
                   climateNames,
-                  coordinateNames = c("x","y"),
-                  neighbourhood.size = 5000, 
-                  no.neighbourhood = T
-)
+                  coordinateNames = c("x","y")
+                  )
 # Save
 save(sai.i, file = "SAIll_4var.data")
 
@@ -221,8 +208,7 @@ for(i in c(20,5000)){
                     scores, 
                     climateNames,
                     coordinateNames = c("x","y"),
-                    neighbourhood.size = i, 
-                    no.neighbourhood = F,
+                    neighbourhood.size = i,
                     standardize = T
   )
   # Save
@@ -246,9 +232,8 @@ for(i in c(20,50,100)){
                     data2 = scores.lgm,
                     climateNames = climateNames,
                     coordinateNames = c("x","y"),
-                    neighbourhood.size = i, 
-                    no.neighbourhood = F
-  )
+                    neighbourhood.size = i
+                    )
   # Save
   save(sai.i, file = paste("SAIcl_", i,"kmNeighbourhood.data", sep=""))
   
@@ -258,9 +243,7 @@ for(i in c(20,50,100)){
 sai.i <- calc_SAI(scores,
                   scores.lgm,
                   climateNames,
-                  coordinateNames = c("x","y"),
-                  neighbourhood.size = 5000, 
-                  no.neighbourhood = T
-)
-save(sai.i, file = "SAIcl_NZ_4var.data")
+                  coordinateNames = c("x","y")
+                  )
+save(sai.i, file = "SAIcl_NZ_4var22sep2019.data")
 
